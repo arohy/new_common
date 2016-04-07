2709a104064b0858c631c83f979d462a1e260f6e
arohy <a.kosheverov@insales.ru>
2016-04-07 15:51:32 +0300

    переписан getProductList
    применен lodash для уменьшения кол-ва велосипедов

--- source/json/getProductsList.js
+++ source/json/getProductsList.js
@@ -2,94 +2,51 @@
  * Получение информации о списке товаров
  */
 
-if (!ISnew.json) {
-  ISnew.json = {}
-};
+ISnew.json.getProductsList = function (id_array) {
+  // указваем, сколько id нужно отправить за раз
+  var query_limit = 25;
 
-ISnew.json.getProductsList = function(id_array) {
-  var end = 0;
-  var paths = [];
-  var ids = [];
-  var productsList;
-
-  /*
-   * хелпер для склейки результатов
-   * target - целевой массив
-   * from - массив из ответа
+  /**
+   * Генерим адреса для запросов
+   *
+   * Приводим к типу массив (мало ли что там прилетело)
+   * Разбиваем на пачки, делаем из них правильные адреса для запросов
    */
-  var concatHelper = function(target, from) {
-    // делаем проверку, к нам пришло описание одного товара или пачки
-    // console.log(from);
-    if (from.product) {
-      target.push(from.product);
-    } else {
-      $.each(from.products, function(index, value) {
-        target.push(value);
-      });
-    }
-
-    return target;
-  };
-
-  // если у водной массив вообще не задан - превращаем его в тыкву.
-  // В результате нам все равно нужен Deferred объект ответа
-  if (id_array === null) {
-    id_array = [];
-  }
-
-  // Распиливаем входной массив на пачки по 25 id'ника
-  paths = id_array.reduce(function(p, c, i) {
-    if (i % 25 === 0) {
-      p.push([]);
-    }
-
-    p[p.length - 1].push(c);
-    return p;
-  }, []);
-
-  // и генерим массив путей для json
-  $.each(paths, function(index, id_list) {
-    paths[index] = '/products_by_id/'+ id_list.join() +'.json';
-  });
+  var paths = _.chain(id_array)
+    .toArray()
+    .compact()
+    .chunk(query_limit)
+    .map(function (ids_list) {
+      return '/products_by_id/'+ ids_list.join() +'.json';
+    })
+    .value();
 
   // собираем задачи
-  promises = $.map(paths, function(path) {
-    var temp = $.ajax(path).then(function(response) {
+  var promises = $.map(paths, function(path) {
+    return $.ajax(path).then(function(response) {
         return response;
       });
-
-    return temp;
   });
 
-  // в качель. микро чит. максимум в запрос - 100 товаров
-  // ну кто еще хочет забрать инфу о более чем 100 товарах,
-  // если в шаблон коллекции за раз отдается максимум 100??
-  // склеиваем ответы
-  productsList = $.when.apply(this, promises)
-    .then(function(response_1, response_2, response_3, response_4) {
-      var result = {
-        products: []
-      };
-
-      //console.log( response_1, response_2, response_3, response_4 );
-      if (response_1) {
-        result.products = concatHelper(result.products, response_1);
-      }
-
-      if (response_2) {
-        result.products = concatHelper(result.products, response_2);
-      }
-
-      if (response_3) {
-        result.products = concatHelper(result.products, response_3);
-      }
-
-      if (response_4) {
-        result.products = concatHelper(result.products, response_4);
-      }
-
-      return result;
+  /**
+   * Склеиваем ответы.
+   *
+   * Проходимся по всем получившимся промисам, дергаем их
+   */
+  return $.when.apply(this, promises)
+    .then(function () {
+      /**
+       * Получаем ответы ото ВСЕХ промисов.
+       * Приводим к типу массив, на случай если ответы все кривые и там - пустота
+       * Вытаскиваем массив с продуктами, склеиваем все массивы и выдаем наружу
+       */
+      return _.chain(arguments)
+        .toArray()
+        .map(function (response) {
+          return response.products;
+        })
+        .flatten()
+        .union()
+        .value()
     });
-
-  return productsList;
 }
