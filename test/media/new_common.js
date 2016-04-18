dfd464c5ed1e52601a90ed4e3a6748c56eec6483
arohy <a.kosheverov@insales.ru>
2016-04-18 15:53:09 +0300

    слияние исправлений с мастера

--- test/media/new_common.js
+++ test/media/new_common.js
@@ -32,37 +32,6 @@ if (!Events) {
   var Events;
 }
 /**
- * Event bus
- *
- * Шина событий. Построена на $.Callbacks;
- */
-
-Events = function (id) {
-  var id = _.toString(id);
-  var Event = id && EventsList[id];
-  var callbacks;
-
-  // Если у нас новое событие, создаем его и объявляем в системе.
-  if (!Event) {
-    // Вешаем флаг того, что во вновь подключенные колбеки будет передана инфа
-    callbacks = $.Callbacks('memory');
-
-    // Объявляем методы
-    Event = {
-      publish: callbacks.fire,
-      subscribe: callbacks.add,
-      unsubscribe: callbacks.remove
-    };
-
-    // Объявляем в системе
-    if (id) {
-      EventsList[id] = Event;
-    }
-  }
-
-  return Event;
-}
-/**
  * Cart
  *
  * Зависит от ISnew.json, Events, ISnew.CartHelper
@@ -327,6 +296,37 @@ ISnew.CartHelper = function () {
     return;
   }
 };
+/**
+ * Event bus
+ *
+ * Шина событий. Построена на $.Callbacks;
+ */
+
+Events = function (id) {
+  var id = _.toString(id);
+  var Event = id && EventsList[id];
+  var callbacks;
+
+  // Если у нас новое событие, создаем его и объявляем в системе.
+  if (!Event) {
+    // Вешаем флаг того, что во вновь подключенные колбеки будет передана инфа
+    callbacks = $.Callbacks('memory');
+
+    // Объявляем методы
+    Event = {
+      publish: callbacks.fire,
+      subscribe: callbacks.add,
+      unsubscribe: callbacks.remove
+    };
+
+    // Объявляем в системе
+    if (id) {
+      EventsList[id] = Event;
+    }
+  }
+
+  return Event;
+}
 /*
  * Добавление товара в корзину
  */
