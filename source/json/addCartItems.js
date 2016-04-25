14da73176bcbbe53e50e8803b0426e0fa2a5f151
arohy <a.kosheverov@insales.ru>
2016-04-25 17:20:28 +0300

    добавлен купон скидок в json корзины

--- source/json/addCartItems.js
+++ source/json/addCartItems.js
@@ -8,16 +8,21 @@
  * Внезапно, если это объект невалидного вида мы все равно получим ответ!!!
  */
 
-ISnew.json.addCartItems = function (items, comments) {
+ISnew.json.addCartItems = function (items, options) {
   var fields = {};
+  options = options || {};
 
   _.forIn(items, function (quantity, variant_id) {
     fields['variant_ids['+ variant_id +']'] = _.toInteger(quantity);
   });
 
-  _.forIn(comments, function (comment, variant_id) {
+  _.forIn(options.comments, function (comment, variant_id) {
     fields['cart[order_line_comments]['+ variant_id +']'] = comment;
   });
+
+  if (options.coupon) {
+    fields['cart[coupon]'] = options.coupon;
+  }
 
   return $.post('/cart_items.json', fields);
 }
--- source/json/updateCartItems.js
+++ source/json/updateCartItems.js
@@ -2,18 +2,23 @@
  * Обновление корзины
  */
 
-ISnew.json.updateCartItems = function (items, comments) {
+ISnew.json.updateCartItems = function (items, options) {
   var fields = {
     '_method': 'put'
   };
+  options = options || {};
 
   _.forIn(items, function(quantity, variant_id) {
     fields['cart[quantity]['+ variant_id +']'] = _.toInteger(quantity);
   });
 
-  _.forIn(comments, function(comment, variant_id) {
+  _.forIn(options.comments, function(comment, variant_id) {
     fields['cart[order_line_comments]['+ variant_id +']'] = comment;
   });
+
+  if (options.coupon) {
+    fields['cart[coupon]'] = coupon;
+  }
 
   return $.post('/cart_items.json', fields);
 };
