38992df15d5b288ba0b1d3e40952aa230ec42df0
arohy <a.kosheverov@insales.ru>
2016-05-24 12:21:40 +0300

    фиксация tools/translit.js

--- source/tools/translit.js
+++ source/tools/translit.js
@@ -3,35 +3,36 @@
  */
 ISnew.tools.Translit = function( string ){
   var self = this;
+
+  var space = '_';
+
+  self.translit = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
+    'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
+    'о': 'o', 'п': 'p', 'р': 'r','с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
+    'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sh','ъ': space, 'ы': 'y', 'ь': space, 'э': 'e', 'ю': 'yu', 'я': 'ya',
+    ' ': space, '_': space, '`': space, '~': space, '!': space, '@': space,
+    '#': space, '$': space, '%': space, '^': space, '&': space, '*': space,
+    '(': space, ')': space,'-': space, '\=': space, '+': space, '[': space,
+    ']': space, '\\': space, '|': space, '/': space,'.': space, ',': space,
+    '{': space, '}': space, '\'': space, '"': space, ';': space, ':': space,
+    '?': space, '<': space, '>': space, '№':space
+  };
 };
 
+/*
+ * Основной метод.
+ */
 ISnew.tools.Translit.prototype.replace = function (string) {
-  var
-    space     = '_',
-    $translit = {
-      'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh',
-      'з': 'z', 'и': 'i', 'й': 'j', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n',
-      'о': 'o', 'п': 'p', 'р': 'r','с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h',
-      'ц': 'c', 'ч': 'ch', 'ш': 'sh', 'щ': 'sh','ъ': space, 'ы': 'y', 'ь': space, 'э': 'e', 'ю': 'yu', 'я': 'ya',
-      ' ': space, '_': space, '`': space, '~': space, '!': space, '@': space,
-      '#': space, '$': space, '%': space, '^': space, '&': space, '*': space,
-      '(': space, ')': space,'-': space, '\=': space, '+': space, '[': space,
-      ']': space, '\\': space, '|': space, '/': space,'.': space, ',': space,
-      '{': space, '}': space, '\'': space, '"': space, ';': space, ':': space,
-      '?': space, '<': space, '>': space, '№':space
-    },
-    result  = '',
-    current = '';
+  var self = this;
 
-  string = string.toLowerCase();
-
-  for(var i = 0; i < string.length; i++ ){
-    if( $translit[ string[ i ] ] !== undefined ){
-      result += $translit[ string[i] ];
-    }else{
-      result += string[ i ];
+  return _.reduce(string.toLowerCase(), function(test, symbol) {
+    if (self.translit[symbol] !== undefined) {
+      test += self.translit[symbol];
+    } else {
+      test += symbol;
     }
-  }
 
-  return result;
+    return test;
+  }, '');
 }
