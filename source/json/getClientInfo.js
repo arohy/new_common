8f2a8a5db10f6f156c4443a362a0770f6b48b760
arohy <a.kosheverov@insales.ru>
2016-05-27 10:50:53 +0300

    изменен ответ от getClientInfo
    возвращает нормально предаставленную информацию
    с указанием авторизован клиент или нет

--- source/json/getClientInfo.js
+++ source/json/getClientInfo.js
@@ -1,3 +1,23 @@
 ISnew.json.getClientInfo = function (){
-  return $.getJSON('/client_account/contacts.json');
+  var result = $.Deferred();
+
+  $.getJSON('/client_account/contacts.json')
+    .done(function (response) {
+      switch (response.status) {
+        case 'error':
+          result.resolve({
+            message: response.message,
+            url: response.url,
+            authorized: false
+          });
+          break;
+        default:
+          result.resolve(_.merge(response.client, { authorized: true }));
+      }
+    })
+    .fail(function (response) {
+      console.log('json.getClientInfo: fail: ', response);
+    });
+
+  return result.promise();
 };
