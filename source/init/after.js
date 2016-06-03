dc43a67351b5721dcd5fad3377738cb498b3c573
arohy <a.kosheverov@insales.ru>
2016-06-03 17:18:29 +0300

    test 3

--- source/init/after.js
+++ source/init/after.js
@@ -11,6 +11,7 @@ var Shop = new ISnew.Shop();
 Site.URL = new ISnew.tools.URL();
 Site.Translit = new ISnew.tools.Translit();
 
+console.log('ready!!!');
 // =======================================================================================
 //                                      OptionSelector
 // =======================================================================================
