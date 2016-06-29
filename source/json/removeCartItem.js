8aadaf70c490e2e868f411880d87af09ca685162
arohy <a.kosheverov@insales.ru>
2016-06-29 15:49:09 +0300

    Учет текущей локали

--- source/json/removeCartItem.js
+++ source/json/removeCartItem.js
@@ -3,8 +3,10 @@
  */
 
 ISnew.json.removeCartItem = function (variant_id) {
+  var URL = new ISnew.tools.URL();
   var path = '/cart_items/'+ _.toInteger(variant_id) +'.json';
   var fields = {
+    lang: URL.getKeyValue('lang'),
     '_method': 'delete'
   };
 
