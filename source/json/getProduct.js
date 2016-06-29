fd4b57f99bd0947d9065f6b8841d9cec1365096d
arohy <a.kosheverov@insales.ru>
2016-06-29 16:01:38 +0300

    json.getProduct: учет текущей локали

--- source/json/getProduct.js
+++ source/json/getProduct.js
@@ -3,9 +3,14 @@
  */
 
 ISnew.json.getProduct = function (id) {
+  var URL = new ISnew.tools.URL();
+  var fields = {
+    format: 'json',
+    lang: URL.getKeyValue('lang')
+  };
   var result = $.Deferred();
 
-  $.getJSON('/product_by_id/'+ _.toInteger(id) +'.json', { format: 'json' })
+  $.getJSON('/product_by_id/'+ _.toInteger(id) +'.json', fields)
     .done(function (response) {
       result.resolve(response.product);
     })
