0ada3999ed767984408792ab9032ae387dc92b21
arohy <a.kosheverov@insales.ru>
2016-07-20 18:45:26 +0300

    Добавлено описание для ajax/message/send

--- source/ajax/message/send.js
+++ source/ajax/message/send.js
@@ -1,11 +1,36 @@
-/*
- * Отправление сообщения
+/**
+ * @module ajaxAPI/message/send
+ * @alias ajaxAPI/shop/message
+ *
+ * @description
+ * Отпаравка сообщений
+ *
+ * @param {Object} options - объект с полями
+ * @param {string} options.content - тело сообщения. Обязательно
+ * @param {string} options.from - e-mail, с которого "отправлено" сообщение. Обязательно
+ * @param {string} options.phone - телефон, указывается в теле письма. По-умолчанию - пустое
+ * @param {string} options.name - имя, указывается в теле письма. По-умолчанию - пустое.
+ * @param {string} options.subject - тема письма.
+ *
+ * @return {$.Deferred} $.promise
+ *
+ * @example
+ * ajaxAPI.shop.message({
+ *   'from': 'json@test.ru',
+ *   'name': 'test is my name',
+ *   'subject': 'test is my subject',
+ *   'content': 'YAAAAR!!!!',
+ *   'phone': '+00000000000000'
+ * })
+ *  .done(function (onDone) { console.log('onDone: ', onDone) })
+ *  .fail(function (onFail) { console.log('onFail: ', onFail) });
  */
-var URL = require('../tools/url');
-var $ = require('jquery');
-var _ = require('lodash');
 
 module.exports = function (input) {
+  var URL = require('../tools/url');
+  var $ = require('jquery');
+  var _ = require('lodash');
+
   var result = $.Deferred();
   var _lang = URL.getKeyValue('lang') || '';
   var message = {
