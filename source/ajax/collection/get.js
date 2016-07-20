d9b2ffad6b611c23112a780c8615a762db696686
arohy <a.kosheverov@insales.ru>
2016-07-20 18:21:39 +0300

    Добавлено описание ajax/collection/get

--- source/ajax/collection/get.js
+++ source/ajax/collection/get.js
@@ -1,11 +1,39 @@
-/*
+/**
+ * @module ajaxAPI/collection/get
+ *
+ * @description
  * Получение информации о коллекции
+ *
+ * @param {string} handle - пермалинк коллекции, объязателен.
+ * @param {Object} filter - объект с выбранными параметрами для фильтрации
+ * @param {Object} pager - объект с настройками пагинации
+ * @param {number} pager.page_size - размер разбивки на страницы
+ * @param {number} pager.page - номер страницы, по которой получаем информацию
+ *
+ * @return {json}
+ * { status: "ok", count: 19, products: [//массив товаров]}
+ *
+ * @example
+ * var filter = {
+ *   price_min: 4000,
+ *   price_max: 10000
+ * };
+ *
+ * var pager = {
+ *   page_size: 25,
+ *   page: 2
+ * }
+ *
+ * ajaxAPI.collection.get('collection_handle', filter, pager)
+ *  .done(function (onDone) { console.log('onDone: ', onDone) })
+ *  .fail(function (onFail) { console.log('onFail: ', onFail) });
  */
-var URL = require('../tools/url');
-var $ = require('jquery');
-var _ = require('lodash');
 
 module.exports = function () {
+  var URL = require('../tools/url');
+  var $ = require('jquery');
+  var _ = require('lodash');
+
   var path = '/collection/'+ _.toString(arguments[0]) +'.json';
   var _lang = URL.getKeyValue('lang') || '';
   var fields = {
