48372b1e1798ee395ef8712707dcdf5e6a6ebb88
arohy <a.kosheverov@insales.ru>
2016-07-20 17:01:30 +0300

    Добавление документации по ajax/cart/remove

--- source/ajax/cart/remove.js
+++ source/ajax/cart/remove.js
@@ -1,11 +1,22 @@
-/*
- * Удаление товара из корзины
+/**
+ * @module ajaxAPI/cart/remove
+ * @description Удаление товара из корзины.
+ *
+ * @param {Number} variant_id - id модификации
+ *
+ * @return {$.ajax} $.post
+ *
+ * @example
+ * ajaxAPI.cart.remove(123123)
+ *  .done(function (onDone) { console.log('onDone: ', onDone) })
+ *  .fail(function (onFail) { console.log('onFail: ', onFail) });
  */
-var URL = require('../tools/url');
-var $ = require('jquery');
-var _ = require('lodash');
 
 module.exports = function (variant_id) {
+  var URL = require('../tools/url');
+  var $ = require('jquery');
+  var _ = require('lodash');
+
   var path = '/cart_items/'+ _.toInteger(variant_id) +'.json';
   var _lang = URL.getKeyValue('lang') || '';
   var fields = {
